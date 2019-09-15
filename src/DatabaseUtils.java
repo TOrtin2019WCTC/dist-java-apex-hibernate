76675f9220c015f06dd278be8b9e9860f9a35b9e
tim <tortin@my.wctc.edu>
2019-09-15 17:15:44 -0500

    added search

---
+++ src/DatabaseUtils.java
@@ -0,0 +1,36 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseUtils {
+    public static final String DRIVER_NAME = "jdbc:derby:";
+    public static final String DATABASE_PATH = "/WEB-INF/lib/ApexLegends";
+    public static final String USERNAME = "tim";
+    public static final String PASSWORD = "tim";
+
+    public static void closeAll(Connection conn, Statement stmt, ResultSet rset) {
+        if (rset != null) {
+            try {
+                rset.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
---
+++ src/searchServlet.java
@@ -0,0 +1,55 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.*;
+
+@WebServlet(name = "searchServlet", urlPatterns = "/search")
+public class searchServlet extends HttpServlet {
+    private final String USER = "tim";
+    private final String PW ="tim";
+    private final String DRIVER = "jdbc:derby:";
+    private final String DB_PATH= "/WEB-INF/lib/apex";
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rset = null;
+
+
+        try{
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+            String path = getServletContext().getRealPath(DB_PATH);
+            conn = DriverManager.getConnection(DRIVER + path, USER, PW);
+
+            pstmt = conn.prepareStatement("SELECT USER_ID FROM USERS WHERE LAST_NAME = ?");
+
+            String searchTerm = request.getParameter("searchTerm");
+
+            pstmt.setString(1,searchTerm);
+
+            rset= pstmt.executeQuery();
+
+            StringBuilder html = new StringBuilder("<html><body>");
+
+            while(rset.next()){
+                int id =  rset.getInt("user_id");
+
+                html.append("<p>").append(id).append("</p>");
+            }
+
+            html.append("</body></html>");
+
+            response.getWriter().print(html.toString());
+        }catch(ClassNotFoundException | SQLException ex){
+            response.getWriter().print(ex.getMessage());
+        }
+
+    }
+    }
+
