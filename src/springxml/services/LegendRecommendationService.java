b8eb5dc544e0f97d89c2464354b7aaf8a89eca59
tim <tortin@my.wctc.edu>
2019-10-05 13:15:28 -0500

    added beans and services

---
+++ src/springxml/services/LegendRecommendationService.java
@@ -0,0 +1,8 @@
+package springxml.services;
+
+public class LegendRecommendationService implements RecommendationService {
+    @Override
+    public String getRecommendation() {
+        return "Crypto";
+    }
+}
---
+++ src/springxml/services/WeaponRecommendationService.java
@@ -0,0 +1,10 @@
+package springxml.services;
+
+public class WeaponRecommendationService implements RecommendationService{
+
+
+    @Override
+    public String getRecommendation() {
+        return "Energy rife";
+    }
+}
